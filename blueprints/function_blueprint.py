80f666ca74b138a72853a45c8a0bc3faee8b1548
shinigamisama <shinigami.sama.mm@gmail.com>
2024-06-04 11:31:38 +0200

    Update function_blueprint.py

--- blueprints/function_blueprint.py
+++ blueprints/function_blueprint.py
@@ -228,7 +228,7 @@ If a function tool doesn't match the query, return an empty string. Else, pick a
         try:
             # Call the OpenAI API to get the function response
             r = requests.post(
-                url=f"{self.valves.OLLAMA_API_BASE_URL}/ollama/api/chat",
+                url=f"{self.valves.OLLAMA_API_BASE_URL}/api/chat",
                 json={
                     "model": self.valves.TASK_MODEL,
                     "messages": [
